b53270e4213b186cb60c2d1dfa35e9eda5447149
XiaoMigros <xiaomigros3@gmail.com>
2023-03-24 17:08:26 +0100

    explain beaming functions

--- presets/musefourBM.js
+++ presets/musefourBM.js
@@ -1,21 +1,22 @@
 //===================================================//
 //MuseScore 4 Beam Mode Conversion for ReBeamer Plugin
 //Copyright (C) 2023 XiaoMigros
-//v 1.0
+//v 1.0.1
 //Changelog:
+//Description of individual beaming functions
 //===================================================//
 
 // MuseScore 4 uses different enumeration values to set beam modes of notes.
 // This function converts them
 
-// MuseScore 3 | MuseScore 4
-//      0      |      0
-//      1      |      2
-//      2      |      5
-//      3      |      6
-//      4      |      1
-//      5      |      3
-//      6      |      4
+// MuseScore 3 | MuseScore 4 | Function
+//      0      |      0      | Auto Beam
+//      1      |      2      | Break beam left
+//      2      |      5      | Join beams
+//      3      |      6      | Beam in groups of 3?
+//      4      |      1      | No beam
+//      5      |      3      | Break inner beams (eighth)
+//      6      |      4      | Break inner beams (sixteenth)
 //      +      |      +
 //      5      |      5
 // (all values >= 7 are treated as beamMode 5)
